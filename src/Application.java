036015f450c4fac07d6e94fef90f7f531722b5ea
Gregory Choice <c9311718@uon.edu.au>
2019-04-17 09:15:54 +1000

    [Feature] Added main runnable class

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+public class Application
+{
+    public void run()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        Application AES = new Application();
+        AES.run();
+    }
+}
